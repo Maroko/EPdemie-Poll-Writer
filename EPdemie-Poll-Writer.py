0c4b38830bb87a9e751fa381279e7e545f4a1983
Matthias Koch <maroko96+github@protonmail.com>
2021-05-04 22:51:26 +0200

    Update

--- EPdemie-Poll-Writer.py
+++ EPdemie-Poll-Writer.py
@@ -1,22 +1,31 @@
 import time
 import keyboard
 
-songsFile = open('songs.txt', 'r')
+writeToFile = True
+poll_text = ""
+
+
+def write_poll(text_to_append, full_text):
+    full_text = full_text + text_to_append
+    return full_text
+
+
+songsFile = open('songs.txt', 'r', encoding='utf8')
 songs = songsFile.read().splitlines()
+
+result = open("result.txt", "w", encoding='utf8')
 
 print("Warte 5 Sekunden vor dem schreiben.")
 for i in range(0, 5):
-    print(5-i)
+    print(5 - i)
     time.sleep(1)
 
-keyboard.write("/poll ")
-time.sleep(0.25)
-keyboard.write("Bester Song? ")
-time.sleep(0.25)
-keyboard.press_and_release('tab')
-time.sleep(0.25)
+poll_text = write_poll("/poll ", poll_text)
+poll_text = write_poll("question:", poll_text)
+poll_text = write_poll("Bester Song? ", poll_text)
 
 index = True
+index_char = 97
 for song in songs:
     if index:
         index = not index
@@ -24,10 +33,13 @@ for song in songs:
     else:
         index = not index
 
-    keyboard.press_and_release('tab')
-    time.sleep(0.5)
-    keyboard.write(song)
-    time.sleep(0.5)
-    keyboard.press_and_release('tab')
-    time.sleep(0.5)
+    poll_text = write_poll("choice_" + chr(index_char) + ":", poll_text)
+    poll_text = write_poll(song + " ", poll_text)
     print("Song: " + song)
+    index_char = index_char + 1
+
+print(poll_text)
+if writeToFile:
+    result.write(poll_text)
+else:
+    keyboard.write(poll_text)
