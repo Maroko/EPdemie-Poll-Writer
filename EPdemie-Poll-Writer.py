15d6ee6fe8dd12cbf2e05d4a22e0ad70ae1c9c25
Matthias Koch <maroko96+github@protonmail.com>
2021-05-11 20:29:16 +0200

    Optionen Clipboard und Skip %2 lines hinzugefügt

--- EPdemie-Poll-Writer.py
+++ EPdemie-Poll-Writer.py
@@ -1,5 +1,6 @@
 import time
 import keyboard
+import pyperclip
 
 writeToFile = True
 poll_text = ""
@@ -15,23 +16,25 @@ songs = songsFile.read().splitlines()
 
 result = open("result.txt", "w", encoding='utf8')
 
-print("Warte 5 Sekunden vor dem schreiben.")
-for i in range(0, 5):
-    print(5 - i)
-    time.sleep(1)
-
 poll_text = write_poll("/poll ", poll_text)
 poll_text = write_poll("question:", poll_text)
 poll_text = write_poll("Bester Song? ", poll_text)
 
+skipSongNumber = input("Skip every second line (song number)? (y/n)")
+if skipSongNumber == "y":
+    skipSongNumber = True
+else:
+    skipSongNumber = False
+
 index = True
 index_char = 97
 for song in songs:
-    if index:
-        index = not index
-        continue
-    else:
-        index = not index
+    if skipSongNumber:
+        if index:
+            index = not index
+            continue
+        else:
+            index = not index
 
     poll_text = write_poll("choice_" + chr(index_char) + ":", poll_text)
     poll_text = write_poll(song + " ", poll_text)
@@ -39,7 +42,18 @@ for song in songs:
     index_char = index_char + 1
 
 print(poll_text)
-if writeToFile:
+answer = input("Write to file? (y/n)")
+if answer == "y":
     result.write(poll_text)
-else:
+
+answer = input("Write to keyboard? (y/n)")
+if answer == "y":
+    print("Warte 5 Sekunden vor dem schreiben.")
+    for i in range(0, 5):
+        print(5 - i)
+        time.sleep(1)
     keyboard.write(poll_text)
+
+answer = input("Copy to clipboard? (y/n)")
+if answer == "y":
+    pyperclip.copy(poll_text)
