dee02693d57dc6f641d1281eb625163a494132eb
Matthias Koch <maroko96+github@protonmail.com>
2021-05-25 21:21:06 +0200

    Autoinstall packages

--- EPdemie-Poll-Writer.py
+++ EPdemie-Poll-Writer.py
@@ -1,6 +1,21 @@
-import time
-import keyboard
-import pyperclip
+try:
+    import time
+except ImportError:
+    from pip._internal import main as pip
+    pip(['install', '--user', 'time'])
+    import time
+try:
+    import keyboard
+except ImportError:
+    from pip._internal import main as pip
+    pip(['install', '--user', 'keyboard'])
+    import keyboard
+try:
+    import pyperclip
+except ImportError:
+    from pip._internal import main as pip
+    pip(['install', '--user', 'pyperclip'])
+    import pyperclip
 
 writeToFile = True
 poll_text = ""
